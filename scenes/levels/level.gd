7453d10c1ecdffc30c4c2ba5a00d1ba9016597bb
Devon Wu <devonwu40@gmail.com>
2025-06-04 00:45:09 -0600

    added projectiles and proper physics for laser

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -1,14 +1,26 @@
 extends Node2D
 
+var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
+var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
 
 func _on_gate_player_entered_gate(body) -> void:
 	print("player entered level gate")
 	print(body)
 
 
-func _on_player_grenade_shot() -> void:
-	print("grenade launched")
+func _on_player_grenade_shot(pos) -> void:
+	var grenade = grenade_scene.instantiate()
+	grenade.position = pos
+	$Projectiles.add_child(grenade)
 
 
-func _on_player_laser_shot() -> void:
-	print("laser shot")
+func _on_player_laser_shot(pos) -> void:
+	
+	var laser = laser_scene.instantiate()
+	laser.position = pos
+	
+	$Projectiles.add_child(laser)
+	# TODO
+	# update laser location
+	# give laser velocity
+	# add laser instance to node2d
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -3,8 +3,8 @@ extends CharacterBody2D
 var can_laser: bool = true
 var can_grenade: bool = true
 
-signal laser_shot()
-signal grenade_shot()
+signal laser_shot(pos)
+signal grenade_shot(pos)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,16 +21,17 @@ func _process(_delta: float) -> void:
 	
 	#shoot laser
 	if Input.is_action_pressed("primary action") and can_laser:
-		#print("shot	")
+		var laser_markers = $LaserStartPositions.get_children()
+		var selected_laser = laser_markers[randi() % laser_markers.size()]
 		$LaserTimer.start()
 		can_laser = false
-		laser_shot.emit()
+		laser_shot.emit(selected_laser.global_position)
 		
 	if Input.is_action_pressed("secondary action") and can_grenade:
-		#print("grenade")
+		var selected_grenade = $GrenadeStartPositions.get_children()[0]
 		$GrenadeTimer.start()
 		can_grenade = false
-		grenade_shot.emit()
+		grenade_shot.emit(selected_grenade.global_position)
 
 
 
