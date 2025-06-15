0c066beab3820b10afac148521b86bbf8b13593e
Devon Wu <devonwu40@gmail.com>
2025-06-15 00:07:39 -0600

    started on UI, currently has template for ammo with count and icon

--- scenes/levels/outside.gd
+++ scenes/levels/outside.gd
@@ -1,16 +1,7 @@
 extends LevelParent
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_gate_player_entered_gate(_body: Variant) -> void:
 	var tween = get_tree().create_tween()
 	tween.tween_property($Player, "speed", 0, 0.5)
+	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
--- scenes/objects/gate.gd
+++ scenes/objects/gate.gd
@@ -3,5 +3,4 @@ extends StaticBody2D
 signal player_entered_gate(body)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print("entered")
 	player_entered_gate.emit(body)
