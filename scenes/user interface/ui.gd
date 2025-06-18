0fa4b09da007ca27663cf7d77d16321584a60a49
Devon Wu <devonwu40@gmail.com>
2025-06-17 23:24:20 -0600

    added setters and getters to global player stats and linked them to update UI whenever modified

--- scenes/user interface/ui.gd
+++ scenes/user interface/ui.gd
@@ -4,14 +4,16 @@ extends CanvasLayer
 @onready var grenade_value: Label = $GrenadeCounter/VBoxContainer/Label
 @onready var laser_icon:TextureRect = $LaserCounter/VBoxContainer/TextureRect
 @onready var grenade_icon:TextureRect = $GrenadeCounter/VBoxContainer/TextureRect
-
+@onready var health_bar:TextureProgressBar = $MarginContainer/TextureProgressBar
 
 var green: Color = Color(0, 1, 0, 1)
 var red: Color = Color(1, 0, 0, 1)
 
 func _ready():
+	Globals.connect("stat_change", update_stat_text)
 	update_grenade_text()
 	update_laser_text()
+	update_health_text()
 
 func update_laser_text():
 	laser_value.text = str(Globals.laser_amount)
@@ -21,6 +23,13 @@ func update_grenade_text():
 	grenade_value.text = str(Globals.grenade_amount)
 	update_color(Globals.grenade_amount, grenade_value, grenade_icon)
 
+func update_health_text():
+	health_bar.value = Globals.health
+
+func update_stat_text():
+	update_laser_text()
+	update_grenade_text()
+	update_health_text()
 
 func update_color(amount:int, label:Label, icon:TextureRect) -> void:
 	if amount <= 0:
