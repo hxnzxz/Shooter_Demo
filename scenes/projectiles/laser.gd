7709652c8cee99a22c9ca9fd3af6fc8d83ecac1c
Devon Wu <devonwu40@gmail.com>
2025-07-01 22:08:42 -0600

    Added mouse cursor, fixed laser sound disappearing when collisoin occurs to early. Somewhat finished, both levels have plenty of enemies. Something to either fix or change, duplicating enemeis means hiting one will trigger the flash effect for all enemies from the same duplication.

--- scenes/projectiles/laser.gd
+++ scenes/projectiles/laser.gd
@@ -17,6 +17,8 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if "hit" in body:
 		body.hit()
+	$Sprite2D.hide()
+	await $AudioStreamPlayer2D.finished
 	queue_free()
 
 
